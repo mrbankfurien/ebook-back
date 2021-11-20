eda31f012caa1a2db956b35e4016a715d6a57d36
mrbankfurien <72947189+mrbankfurien@users.noreply.github.com>
2021-11-20 16:33:11 +0000

    banks

--- dbase/config.js
+++ dbase/config.js
@@ -3,11 +3,11 @@ require('dotenv').config() ;
 
 const pool = mysql.createPool({
 
-	connectionLimit : 2000 ,
-	host            : 'localhost',
-	user            : 'root',
-	password        : '',
-	database        : 'ebook'
+	connectionLimit : process.env.CONECTION_LIMIT ,
+	host            : process.env.DB_HOST,
+	user            : process.env.DB_USER,
+	password        : process.env.DB_PASSWORD,
+	database        : process.env.DB_DATABASE
 
 }) ;
 
